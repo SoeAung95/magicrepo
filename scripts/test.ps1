ff4fe903de317253851d4bb964912003b0df90a2
SoeAung95 <soeaung2981995@gmail.com>
2025-06-17 01:58:47 +0630

    🧪 Add test script for modules

---
+++ scripts/test.ps1
@@ -0,0 +1,7 @@
+Write-Host '?? Testing Grass Miner...'
+Start-Sleep -Milliseconds 500
+Write-Host '? Grass Miner is responding!'
+
+Write-Host '?? Testing GPT CLI...'
+Start-Sleep -Milliseconds 500
+Write-Host '? GPT CLI is ready!'
